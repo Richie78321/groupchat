4e94407ea3ccf6eabfc0c660e9d5428acfdd4c8b
Richie Goulazian <rgoulazian@gmail.com>
2023-01-30 21:01:09 -0500

    Add initial implementation (haven't tested yet)

---
+++ server/chatdata/interface.go
@@ -0,0 +1,73 @@
+package chatdata
+
+import (
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+type Manager interface {
+	GetLock() sync.Locker
+
+	GetOrCreateRoom(roomName string) Chatroom
+}
+
+type Chatroom interface {
+	GetLock() sync.Locker
+
+	SignalSubscriptions()
+	AddSubscription(Subscription)
+	RemoveSubscription(uuid.UUID)
+}
+
+type Message interface {
+}
+
+type Subscription interface {
+	Id() uuid.UUID
+	Username() string
+
+	SignalUpdate()
+	ShouldUpdate() <-chan struct{}
+}
+
+type subscription struct {
+	id       uuid.UUID
+	username string
+
+	// A channel for signalling with a strict buffer size of 1.
+	update chan struct{}
+}
+
+func NewSubscription(username string) Subscription {
+	return &subscription{
+		id:       uuid.New(),
+		username: username,
+
+		// The buffer size is strictly 1 to ensure proper signalling behavior.
+		update: make(chan struct{}, 1),
+	}
+}
+
+func (s *subscription) Id() uuid.UUID {
+	return s.id
+}
+
+func (s *subscription) Username() string {
+	return s.username
+}
+
+func (s *subscription) SignalUpdate() {
+	select {
+	case s.update <- struct{}{}:
+	default:
+		// The update channel has a strict buffer size of 1.
+		// If there is already a signal in the subscription update channel,
+		// then we can safely continue because the subscription has already
+		// been signalled to update.
+	}
+}
+
+func (s *subscription) ShouldUpdate() <-chan struct{} {
+	return s.update
+}
--- server/server.go
+++ server/server.go
@@ -6,12 +6,63 @@ import (
 	"time"
 
 	pb "github.com/Richie78321/groupchat/chatservice"
+	"github.com/Richie78321/groupchat/server/chatdata"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/keepalive"
 )
 
 type chatServer struct {
+	manager chatdata.Manager
 	pb.UnimplementedChatServiceServer
+}
+
+func newChatServer() *chatServer {
+	return &chatServer{
+		manager: chatdata.NewMemoryManager(),
+	}
+}
+
+func (s *chatServer) sendSubscriptionUpdate(stream pb.ChatService_SubscribeChatroomServer) error {
+	return stream.Send(&pb.ChatroomSubscriptionUpdate{})
+}
+
+func (s *chatServer) SubscribeChatroom(req *pb.SubscribeChatroomRequest, stream pb.ChatService_SubscribeChatroomServer) error {
+	// Get or create the requested chatroom
+	var chatroom chatdata.Chatroom
+	{
+		s.manager.GetLock().Lock()
+		defer s.manager.GetLock().Unlock()
+		chatroom = s.manager.GetOrCreateRoom(req.Chatroom.Name)
+	}
+
+	subscription := chatdata.NewSubscription(req.Self.Username)
+	// Trigger this subscription to send an initial update
+	subscription.SignalUpdate()
+
+	// Add this subscription to the chatroom's current subscriptions
+	{
+		chatroom.GetLock().Lock()
+		defer chatroom.GetLock().Unlock()
+		chatroom.AddSubscription(subscription)
+	}
+
+	// Remove this subscription from the chatroom's current subscriptions at exit
+	defer func() {
+		chatroom.GetLock().Lock()
+		defer chatroom.GetLock().Unlock()
+		chatroom.RemoveSubscription(subscription.Id())
+	}()
+
+	for {
+		select {
+		case <-subscription.ShouldUpdate():
+			if err := s.sendSubscriptionUpdate(stream); err != nil {
+				return err
+			}
+		case <-stream.Context().Done():
+			return nil
+		}
+	}
 }
 
 func Start(serverAddress string) error {
@@ -26,7 +77,7 @@ func Start(serverAddress string) error {
 		Timeout: 30 * time.Second,
 	}))
 
-	pb.RegisterChatServiceServer(grpcServer, new(chatServer))
+	pb.RegisterChatServiceServer(grpcServer, newChatServer())
 
 	fmt.Println("Running server...")
 	grpcServer.Serve(lis)
