36d5a0cf3448ca62d3536086aa67ef429bce8e5e
Richie Goulazian <rgoulazian@gmail.com>
2023-02-01 18:03:29 +0000

    Add basic flags for server CLI

--- cmd/servercli/servercli.go
+++ cmd/servercli/servercli.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 
 	"github.com/Richie78321/groupchat/server"
 )
 
 func main() {
-	if err := server.Start("localhost:3000"); err != nil {
+	address := flag.String("address", "localhost", "the server address")
+	port := flag.Int("port", 3000, "the server port")
+	flag.Parse()
+
+	if err := server.Start(fmt.Sprintf("%s:%d", *address, *port)); err != nil {
 		log.Fatalf("%v", err)
 	}
 }
