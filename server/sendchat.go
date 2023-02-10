d3d45239300f77b52627d83f67673fd30422b873
Richie Goulazian <rgoulazian@gmail.com>
2023-02-09 19:58:19 -0500

    Remove unecessary helper...

--- server/sendchat.go
+++ server/sendchat.go
@@ -2,26 +2,8 @@ package server
 
 import (
 	pb "github.com/Richie78321/groupchat/chatservice"
-	"github.com/Richie78321/groupchat/server/chatdata"
 	"golang.org/x/net/context"
 )
-
-func sendChatHelper(ctx context.Context, c chatdata.Chatroom, req *pb.SendChatRequest) (*pb.SendChatResponse, error) {
-	c.GetLock().Lock()
-	// Helper created to use defer on unlock.
-	defer c.GetLock().Unlock()
-
-	// Ensure the user is logged in
-	err := ensureUserLoggedIn(c, req.Self)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create the message and append it to the chatroom
-	c.AppendMessage(req.Self, req.Body)
-
-	return &pb.SendChatResponse{}, nil
-}
 
 func (s *chatServer) SendChat(ctx context.Context, req *pb.SendChatRequest) (*pb.SendChatResponse, error) {
 	// Get the chatroom if it exists
@@ -32,5 +14,17 @@ func (s *chatServer) SendChat(ctx context.Context, req *pb.SendChatRequest) (*pb
 		return nil, err
 	}
 
-	return sendChatHelper(ctx, chatroom, req)
+	chatroom.GetLock().Lock()
+	defer chatroom.GetLock().Unlock()
+
+	// Ensure the user is logged in
+	err = ensureUserLoggedIn(chatroom, req.Self)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the message and append it to the chatroom
+	chatroom.AppendMessage(req.Self, req.Body)
+
+	return &pb.SendChatResponse{}, nil
 }
