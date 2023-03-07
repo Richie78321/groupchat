9d0c6035587409627ae54f1c264f22f7e54f0efd
Richie Goulazian <rgoulazian@gmail.com>
2023-03-07 18:08:26 +0000

    Add basic event updates to replication server

--- server/replicationserver/replicationserver.go
+++ server/replicationserver/replicationserver.go
@@ -1,17 +1,42 @@
 package replicationserver
 
 import (
+	"sync"
+
 	pb "github.com/Richie78321/groupchat/chatservice"
 	"github.com/Richie78321/groupchat/server/replication"
 )
 
 type ReplicationServer struct {
+	lock sync.Mutex
+
+	subscriptions map[*subscription]struct{}
+
 	peerManager *replication.PeerManager
 	pb.UnimplementedReplicationServiceServer
 }
 
+func (r *ReplicationServer) BroadcastEvents(events []*pb.Event) {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
+	for subscription, _ := range r.subscriptions {
+		subscription.broadcastEvents(events)
+	}
+}
+
+func (r *ReplicationServer) addSubscription(s *subscription) {
+	r.subscriptions[s] = struct{}{}
+}
+
+func (r *ReplicationServer) removeSubscription(s *subscription) {
+	delete(r.subscriptions, s)
+}
+
 func NewReplicationServer(peerManager *replication.PeerManager) *ReplicationServer {
 	return &ReplicationServer{
-		peerManager: peerManager,
+		lock:          sync.Mutex{},
+		subscriptions: make(map[*subscription]struct{}),
+		peerManager:   peerManager,
 	}
 }
--- server/replicationserver/replicationsubscribe.go
+++ server/replicationserver/replicationsubscribe.go
@@ -2,14 +2,59 @@ package replicationserver
 
 import (
 	"log"
-	"time"
 
 	pb "github.com/Richie78321/groupchat/chatservice"
 )
 
+type subscription struct {
+	events chan []*pb.Event
+}
+
+func newSubscription() *subscription {
+	return &subscription{
+		events: make(chan []*pb.Event),
+	}
+}
+
+func (s *subscription) broadcastEvents(events []*pb.Event) {
+	// Place the events on the events channel to trigger a subscription update
+	s.events <- events
+}
+
+func sendSubscriptionUpdate(newEvents []*pb.Event, stream pb.ReplicationService_SubscribeUpdatesServer) error {
+	return stream.Send(&pb.SubscriptionUpdate{
+		EphemeralState: &pb.EphemeralState{},
+		Events:         make([]*pb.Event, 0),
+	})
+}
+
 func (s *ReplicationServer) SubscribeUpdates(req *pb.SubscribeRequest, stream pb.ReplicationService_SubscribeUpdatesServer) error {
-	// TODO(richie): Update this to something meaningful
+	subscription := newSubscription()
+
+	// Add this subscription to the current subscriptions
+	s.lock.Lock()
+	s.addSubscription(subscription)
+	s.lock.Unlock()
+
 	log.Printf("Peer subscribed")
-	time.Sleep(10 * time.Hour)
-	return nil
+
+	// Remove this subscription from the current subscriptions at exit
+	defer func() {
+		s.lock.Lock()
+		s.removeSubscription(subscription)
+		s.lock.Unlock()
+	}()
+
+	for {
+		select {
+		case newEvents := <-subscription.events:
+			// When there are new events, send an update over the server stream
+			if err := sendSubscriptionUpdate(newEvents, stream); err != nil {
+				log.Printf("%v", err)
+				return err
+			}
+		case <-stream.Context().Done():
+			return nil
+		}
+	}
 }
