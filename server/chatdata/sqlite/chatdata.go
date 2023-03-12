b2bf0590b1532382d920c4efa59819f705555c4a
Richie Goulazian <rgoulazian@gmail.com>
2023-03-12 20:31:47 +0000

    Add ability to load sequence number from disk

--- server/chatdata/sqlite/chatdata.go
+++ server/chatdata/sqlite/chatdata.go
@@ -31,11 +31,14 @@ type MessageEvent struct {
 }
 
 type SqliteChatdata struct {
-	Lock sync.Mutex
-	db   *gorm.DB
+	Lock                 sync.Mutex
+	db                   *gorm.DB
+	pid                  string
+	nextSequenceNumber   int64
+	nextLamportTimestamp int64
 }
 
-func NewSqliteChatdata(dbPath string) (*SqliteChatdata, error) {
+func NewSqliteChatdata(dbPath string, pid string) (*SqliteChatdata, error) {
 	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
 	if err != nil {
 		return nil, err
@@ -45,10 +48,35 @@ func NewSqliteChatdata(dbPath string) (*SqliteChatdata, error) {
 	db.AutoMigrate(&Event{})
 	db.AutoMigrate(&MessageEvent{})
 
-	return &SqliteChatdata{
-		Lock: sync.Mutex{},
-		db:   db,
-	}, nil
+	c := &SqliteChatdata{
+		Lock:                 sync.Mutex{},
+		db:                   db,
+		pid:                  pid,
+		nextSequenceNumber:   0,
+		nextLamportTimestamp: 0,
+	}
+	if err = c.loadFromDisk(); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func (c *SqliteChatdata) loadFromDisk() error {
+	// Load the next sequence number from disk.
+	selectedEvent := &Event{}
+	result := c.db.Order("sequence_number DESC").Where("pid = ?", c.pid).Limit(1).Find(selectedEvent)
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected > 0 {
+		// Set the next sequence number as the current maximum sequence number plus 1
+		c.nextSequenceNumber = selectedEvent.SequenceNumber + 1
+	}
+
+	// TODO: Load the Lamport Timestamp
+
+	return nil
 }
 
 func (c *SqliteChatdata) ConsumeEvent(event *pb.Event) (bool, error) {
@@ -69,6 +97,12 @@ func (c *SqliteChatdata) ConsumeEvent(event *pb.Event) (bool, error) {
 		Pid:              event.Pid,
 		SequenceNumber:   event.SequenceNumber,
 		LamportTimestamp: event.LamportTimestamp,
+	}
+
+	// Update this server's Lamport Timestamp if this event has a new maximum timestamp.
+	newLts := dbEvent.LamportTimestamp + 1
+	if c.nextLamportTimestamp < newLts {
+		c.nextLamportTimestamp = newLts
 	}
 
 	switch e := event.Event.(type) {
--- server/chatdata/sqlite/chatdata_test.go
+++ server/chatdata/sqlite/chatdata_test.go
@@ -4,21 +4,21 @@ import (
 	"testing"
 
 	pb "github.com/Richie78321/groupchat/chatservice"
+	"github.com/stretchr/testify/assert"
 )
 
-func makeChatdata(t *testing.T) *SqliteChatdata {
-	chatdata, err := NewSqliteChatdata(":memory:")
-	if err != nil {
-		t.Fatal(err)
-	}
+func makeChatdata(t *testing.T, pid string) *SqliteChatdata {
+	chatdata, err := NewSqliteChatdata(":memory:", pid)
+	assert.NoError(t, err)
 
 	return chatdata
 }
 
 func TestIgnoreDuplicates(t *testing.T) {
-	chatdata := makeChatdata(t)
+	pid := "server1"
+	chatdata := makeChatdata(t, pid)
 	testEvent1 := &pb.Event{
-		Pid:              "server1",
+		Pid:              pid,
 		SequenceNumber:   0,
 		LamportTimestamp: 0,
 		Event: &pb.Event_MessageAppend{
@@ -31,23 +31,44 @@ func TestIgnoreDuplicates(t *testing.T) {
 		},
 	}
 	testEvent2 := &pb.Event{
-		Pid:              "server1",
+		Pid:              pid,
 		SequenceNumber:   0,
 		LamportTimestamp: 78321,
 		Event:            &pb.Event_MessageAppend{},
 	}
 
 	// First event should be successfully consumed.
-	if _, err := chatdata.ConsumeEvent(testEvent1); err != nil {
-		t.Fatal(err)
-	}
+	_, err := chatdata.ConsumeEvent(testEvent1)
+	assert.NoError(t, err)
 
 	// Second duplicate event should not cause any failures.
 	ignored, err := chatdata.ConsumeEvent(testEvent2)
-	if err != nil {
-		t.Fatal(err)
+	assert.NoError(t, err)
+	assert.True(t, ignored, "Expected the duplicate event to be ignored")
+}
+
+func TestLTSUpdated(t *testing.T) {
+
+}
+
+func TestLoadFromDisk(t *testing.T) {
+	pid := "server1"
+	chatdata := makeChatdata(t, pid)
+	sequenceNumber := int64(100)
+	lamportTimestamp := int64(200)
+	testEvent := &Event{
+		Pid:              pid,
+		SequenceNumber:   sequenceNumber,
+		LamportTimestamp: lamportTimestamp,
 	}
-	if !ignored {
-		t.Fatal("Expected the duplicate event to be ignored")
-	}
+	// Add an event to the database
+	err := chatdata.db.Create(testEvent).Error
+	assert.NoError(t, err)
+	// Reset the sequence number and LTS
+	chatdata.nextSequenceNumber = 0
+	chatdata.nextLamportTimestamp = 0
+
+	chatdata.loadFromDisk()
+
+	assert.Equal(t, sequenceNumber+1, chatdata.nextSequenceNumber)
 }
