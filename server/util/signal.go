4e964f1f473936451f93c47b4fcf9461a8d69c14
Richie Goulazian <rgoulazian@gmail.com>
2023-04-06 20:12:49 +0000

    Add general signal util

---
+++ server/util/signal.go
@@ -0,0 +1,28 @@
+package util
+
+type Signal struct {
+	// A channel for signalling. Needs a strict buffer size of 1.
+	signal chan struct{}
+}
+
+func NewSignal() Signal {
+	return Signal{
+		// The buffer size is strictly 1 to ensure proper signalling behavior.
+		signal: make(chan struct{}, 1),
+	}
+}
+
+func (s *Signal) Signal() {
+	select {
+	case s.signal <- struct{}{}:
+	default:
+		// The signal channel has a strict buffer size of 1.
+		// If there is already a signal in the channel,
+		// then we can safely continue because the channel has already
+		// been signalled.
+	}
+}
+
+func (s *Signal) GetSignal() <-chan struct{} {
+	return s.signal
+}
