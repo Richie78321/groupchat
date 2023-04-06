199440a685062bceb3540deb153824409a8ff5ce
Richie Goulazian <rgoulazian@gmail.com>
2023-04-06 20:18:55 +0000

    Use new Signal util in subscription

--- server/chatdata/subscription.go
+++ server/chatdata/subscription.go
@@ -2,6 +2,7 @@ package chatdata
 
 import (
 	pb "github.com/Richie78321/groupchat/chatservice"
+	"github.com/Richie78321/groupchat/server/util"
 	"github.com/google/uuid"
 )
 
@@ -9,17 +10,14 @@ type subscription struct {
 	id   uuid.UUID
 	user *pb.User
 
-	// A channel for signalling updates. Needs a strict buffer size of 1.
-	update chan struct{}
+	update util.Signal
 }
 
 func NewSubscription(user *pb.User) Subscription {
 	return &subscription{
-		id:   uuid.New(),
-		user: user,
-
-		// The buffer size is strictly 1 to ensure proper signalling behavior.
-		update: make(chan struct{}, 1),
+		id:     uuid.New(),
+		user:   user,
+		update: util.NewSignal(),
 	}
 }
 
@@ -32,16 +30,9 @@ func (s *subscription) User() *pb.User {
 }
 
 func (s *subscription) SignalUpdate() {
-	select {
-	case s.update <- struct{}{}:
-	default:
-		// The update channel has a strict buffer size of 1.
-		// If there is already a signal in the subscription update channel,
-		// then we can safely continue because the subscription has already
-		// been signalled to update.
-	}
+	s.update.Signal()
 }
 
 func (s *subscription) ShouldUpdate() <-chan struct{} {
-	return s.update
+	return s.update.GetSignal()
 }
